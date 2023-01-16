03160a541132c3711fed8a0dd4163a6f58d7f6ec
LorenzoNicco <lorenzoniccolai91@gmail.com>
2023-01-16 15:57:37 +0100

    Add milestone 1 e ver. alt. di milestone 0

--- js/script.js
+++ js/script.js
@@ -13,48 +13,84 @@
 console.log("Collegamento  file js ok");
 
 // Milestone 0
-const teamMember1 = {
-    completeName: "Wayne Barnett",
-    role: "Founder & CEO",
-    profilePic: "wayne-barnett-founder-ceo.jpg"
-}
-console.log("Wayne", teamMember1);
+// const teamMember1 = {
+//     completeName: "Wayne Barnett",
+//     role: "Founder & CEO",
+//     profilePic: "wayne-barnett-founder-ceo.jpg"
+// }
 
-const teamMember2 = {
-    completeName: "Angela Caroll",
-    role: "Chief Editor",
-    profilePic: "angela-caroll-chief-editor.jpg"
-}
-console.log("Angela", teamMember2);
+// const teamMember2 = {
+//     completeName: "Angela Caroll",
+//     role: "Chief Editor",
+//     profilePic: "angela-caroll-chief-editor.jpg"
+// }
 
-const teamMember3 = {
-    completeName: "Walter Gordon",
-    role: "Office Manager",
-    profilePic: "walter-gordon-office-manager.jpg"
-}
-console.log("Walter", teamMember3);
+// const teamMember3 = {
+//     completeName: "Walter Gordon",
+//     role: "Office Manager",
+//     profilePic: "walter-gordon-office-manager.jpg"
+// }
 
-const teamMember4 = {
-    completeName: "Angela Lopez",
-    role: "Social Media Manager",
-    profilePic: "angela-lopez-social-media-manager.jpg"
-}
-console.log("Angela", teamMember4);
+// const teamMember4 = {
+//     completeName: "Angela Lopez",
+//     role: "Social Media Manager",
+//     profilePic: "angela-lopez-social-media-manager.jpg"
+// }
 
-const teamMember5 = {
-    completeName: "Scott Estrada",
-    role: "Developer",
-    profilePic: "scott-estrada-developer.jpg"
-}
-console.log("Scott", teamMember5);
+// const teamMember5 = {
+//     completeName: "Scott Estrada",
+//     role: "Developer",
+//     profilePic: "scott-estrada-developer.jpg"
+// }
 
-const teamMember6 = {
-    completeName: "Barbara Ramos",
-    role: "Graphic Designer",
-    profilePic: "barbara-ramos-graphic-designer.jpg"
-}
-console.log("Barbara", teamMember6);
+// const teamMember6 = {
+//     completeName: "Barbara Ramos",
+//     role: "Graphic Designer",
+//     profilePic: "barbara-ramos-graphic-designer.jpg"
+// }
 
-const teamMembers = [];
-teamMembers.push(teamMember1, teamMember2, teamMember3, teamMember4, teamMember5, teamMember6);
+// const teamMembers = [];
+// teamMembers.push(teamMember1, teamMember2, teamMember3, teamMember4, teamMember5, teamMember6);
+// console.log("Team", teamMembers);
+
+// Versione alternativa Milestone 0
+const teamMembers = [
+    {
+        completeName: "Wayne Barnett",
+        role: "Founder & CEO",
+        profilePic: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        completeName: "Angela Caroll",
+        role: "Chief Editor",
+        profilePic: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        completeName: "Walter Gordon",
+        role: "Office Manager",
+        profilePic: "walter-gordon-office-manager.jpg"
+    },
+    {
+        completeName: "Angela Lopez",
+        role: "Social Media Manager",
+        profilePic: "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        completeName: "Scott Estrada",
+        role: "Developer",
+        profilePic: "scott-estrada-developer.jpg"
+    },
+    {
+        completeName: "Barbara Ramos",
+        role: "Graphic Designer",
+        profilePic: "barbara-ramos-graphic-designer.jpg"
+    }
+];
 console.log("Team", teamMembers);
+
+// Milestone 1
+for (let i = 0; i < teamMembers.length; i++) {
+    for (let key in teamMembers[i]) {
+        console.log("key", teamMembers[i][key]);
+    }
+}
