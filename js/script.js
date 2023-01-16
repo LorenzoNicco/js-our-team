3c095a5043f6cae9da07e99824b609543a417e39
LorenzoNicco <lorenzoniccolai91@gmail.com>
2023-01-16 15:22:17 +0100

    Add steps tramite commenti e verifica collegamento

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/*
+    STEPS:
+        Milestone 0:
+            1. Creare un oggetto con i dati forniti
+            2. Raggruppare gli oggetti creati in un array.
+        Milestone 1:
+            1. Stampare un console.log di ogni membro del team con le proprie informazioni.
+            2. Per farlo, creare un ciclo per stampare in console
+        Milestone 2:
+            1. Stampare le stesse informazioni in pagina, sottoforma di stringhe
+*/
+console.log("Collegamento  file js ok");
